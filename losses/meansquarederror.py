86b1b45ab36dc877bb33059584d5206a162d611c
Sarvin Baghi <87063732+SarvinB@users.noreply.github.com>
2023-07-04 01:17:43 +0330

    Update meansquarederror.py

--- losses/meansquarederror.py
+++ losses/meansquarederror.py
@@ -14,8 +14,8 @@ class MeanSquaredError:
                 mean squared error loss
         """
         # TODO: Implement mean squared error loss
-        batch_size = None
-        cost = None
+        batch_size = y_pred.shape[1]
+        cost = np.sum((y_true-y_pred)*(y_true-y_pred))/batch_size
         return np.squeeze(cost)
     
     def backward(self, y_pred, y_true):
@@ -28,4 +28,4 @@ class MeanSquaredError:
                 derivative of the mean squared error loss
         """
         # TODO: Implement backward pass for mean squared error loss
-        return None
+        return 2*(y_pred-y_true)/y_pred.shape[1]
